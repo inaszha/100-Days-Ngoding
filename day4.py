a2394e8edabdb4cdcd8af16c41e4cca4b507d311
inaszha <99781124+inaszha@users.noreply.github.com>
2023-10-12 22:15:52 +0800

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,17 @@
+today = "Rabu"
+uts_semantik = "belum belajar"
+
+print("Hari ini adalah", today)
+print("Besok adalah hari Kamis dan saya", uts_semantik, "untuk UTS semantik.")
+
+while today != "Kamis" and uts_semantik == "belum belajar":
+    print("saya merasa khawatir karena besok adalah hari Kamis dan saya belum belajar.")
+    print("saya selalu menyesal diakhir karena memilih untuk memakai sistem kebut semalam.")
+
+    # Simulasikan waktu berlalu
+    today = "Kamis"
+    uts_semantik = "sedang belajar"
+
+print("Hari ini adalah", today)
+print("Besok adalah hari Kamis dan saya", uts_semantik, "untuk UTS semantik.")
+print("Anda telah memutuskan untuk belajar, jadi semoga Tuhan dan alam berpihak kepada saya.!")
