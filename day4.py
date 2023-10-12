8cf2b9dddd5b95e2e9ad9b0d7e936b09648d8bd5
inaszha <99781124+inaszha@users.noreply.github.com>
2023-10-12 22:24:21 +0800

    day4.py

--- day4.py
+++ day4.py
@@ -1,17 +1,17 @@
-today = "Rabu"
+today = "kamis"
 uts_semantik = "belum belajar"
 
 print("Hari ini adalah", today)
-print("Besok adalah hari Kamis dan saya", uts_semantik, "untuk UTS semantik.")
+print("Besok adalah hari jumat dan saya", uts_semantik, "untuk UTS semantik.")
 
-while today != "Kamis" and uts_semantik == "belum belajar":
-    print("saya merasa khawatir karena besok adalah hari Kamis dan saya belum belajar.")
+while today != "jumat" and uts_semantik == "belum belajar":
+    print("saya merasa khawatir karena besok adalah hari jumat dan saya belum belajar.")
     print("saya selalu menyesal diakhir karena memilih untuk memakai sistem kebut semalam.")
 
     # Simulasikan waktu berlalu
-    today = "Kamis"
+    today = "jumat"
     uts_semantik = "sedang belajar"
 
 print("Hari ini adalah", today)
-print("Besok adalah hari Kamis dan saya", uts_semantik, "untuk UTS semantik.")
+print("Besok adalah hari jumat  dan saya", uts_semantik, "untuk UTS semantik.")
 print("saya telah memutuskan untuk belajar, jadi semoga Tuhan dan alam berpihak kepada saya.!")
