df656a0692abec51fc643a1e34b6845921066a4b
inaszha <99781124+inaszha@users.noreply.github.com>
2023-10-25 23:11:11 +0800

    Add files via upload

---
+++ day17.py
@@ -0,0 +1,7 @@
+ingin_petualangan = input("cape y? (ya/tidak): ")
+
+# Kondisi untuk menentukan apakah petualangan adalah mimpi atau kenyataan
+if ingin_petualangan == "ya":
+    print("ya :' ")
+else:
+    print("ku kuat")
