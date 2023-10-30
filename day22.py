4d24a9deea36180011137affb4c675f3e8761acf
inaszha <99781124+inaszha@users.noreply.github.com>
2023-10-30 23:47:10 +0800

    Add files via upload

---
+++ day22.py
@@ -0,0 +1,11 @@
+JenisBuku = input("Masukkan Jenis Buku : ")
+AreaPengambilan = input("Masukkan Area Pengambilan : ")
+
+if JenisBuku == "Novel" and AreaPengambilan == "A":
+    print("Benar")
+elif JenisBuku == "Komik" and AreaPengambilan == "B":
+    print("Benar")
+elif JenisBuku == "Majalah" and AreaPengambilan == "C":
+    print("Benar")
+else:
+    print("Salah")
