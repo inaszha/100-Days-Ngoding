b0b72fe567477383748d0ab156c6638765dca944
inaszha <99781124+inaszha@users.noreply.github.com>
2023-10-31 22:01:32 +0800

    Add files via upload

---
+++ day23.py
@@ -0,0 +1,11 @@
+harga_mobil = 150000000
+penghasilan = 6000000
+kebutuhan = 3000000
+ortu = 1000000
+
+tabungan = penghasilan - kebutuhan - ortu
+lamaNabung_bulan = harga_mobil / tabungan
+lamaNabung_tahun = lamaNabung_bulan / 12
+
+print("uang yang dapat ditabung tiap bulan senilai:", tabungan)
+print("lama waktu menabung untuk membeli sebuah mobil adalah:", lamaNabung_tahun, "tahun")
