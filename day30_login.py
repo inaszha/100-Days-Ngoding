045b2d19fccf155c89ff60af37eae7c1717d6820
inaszha <99781124+inaszha@users.noreply.github.com>
2023-11-07 23:36:04 +0800

    Add files via upload

---
+++ day30_login.py
@@ -0,0 +1,7 @@
+user = str (input("masukkan username :"))
+password = int (input("masukkan password :"))
+
+if user == "inas14" and password == 123:
+    print ("anda berhasil login")
+else:
+    print("username atau password anda salah")
