fb84e951938916b63199ef37c2b204feb2223ab5
inaszha <99781124+inaszha@users.noreply.github.com>
2023-11-10 23:09:11 +0800

    Add files via upload

---
+++ day33_IO.py
@@ -0,0 +1,10 @@
+a = int (input("masukkan angka 1 :"))
+b = int (input("masukkan angka 2 :"))
+c = a + b
+
+if c % 2 == 0:
+    d = c + a
+    print ("hasilnya ditambah dengan angka 1 maka",d)
+else:
+    e = c + b
+    print ("hasilnya ditambah dengan angka 1 maka",e)
