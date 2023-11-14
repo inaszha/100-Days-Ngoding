fbacf0ed6a43cc3031865f64926e7efa742efa84
inaszha <99781124+inaszha@users.noreply.github.com>
2023-11-14 23:52:18 +0800

    Add files via upload

---
+++ day37.py
@@ -0,0 +1,10 @@
+a = int(input("masukkan nilai a : "))
+b = int(input("masukkan nilai b : "))
+c= a * b
+
+if c % 2 == 0 and c-3 != 0 :
+    d = c*2
+    print ("hasil :",d)
+else:
+    d = c+2
+    print ("hasil :",d)
