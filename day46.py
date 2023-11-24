9f2aeadc77b02c6895f62a3a9fd28b1333d86aee
inaszha <99781124+inaszha@users.noreply.github.com>
2023-11-24 23:52:59 +0800

    Add files via upload

---
+++ day46.py
@@ -0,0 +1,10 @@
+a = int(input("masukkan angka 1 :"))
+b = int(input("masukkan angka 2 :"))
+c = a + b
+
+if c % 2 == 0:
+    d = c + 1
+    print ("hasil",d)
+else:
+    d = c + 2
+    print ("hasil",d)
