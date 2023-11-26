97b5dd98952d7d4a38ba3408306699cec10b8c21
inaszha <99781124+inaszha@users.noreply.github.com>
2023-11-26 22:43:45 +0800

    Add files via upload

---
+++ day49.py
@@ -0,0 +1,13 @@
+array = [1 , 2 , 3 , 4 , 5]
+
+print("array awal :", array)
+
+array[-1]=10
+array[-2]=9
+
+print ("array setelah di update :",array)
+
+del array[-1]
+del array[-2]
+
+print ("array setelah di delete :",array)
