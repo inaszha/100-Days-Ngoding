db262f0e37bfff583692beaa81f39e47b74b1d09
inaszha <99781124+inaszha@users.noreply.github.com>
2023-12-17 09:04:29 +0800

    Add files via upload

---
+++ day69.py
@@ -0,0 +1,9 @@
+suka_bubur_ayam = input("Apakah Anda suka bubur ayam? : ")
+if suka_bubur_ayam == "Ya":
+    suka_bubur_ayam_diaduk = input("Apakah Anda suka bubur ayam yang diaduk? (Ya/Tidak): ")
+    if suka_bubur_ayam_diaduk == "Tidak":
+        print("jiwa kemanusiaan anda masih terjaga")
+    else:
+        print("cek mental health anda")
+else:
+    print("merugi adalah orang orang yang tidak suka buryam")
