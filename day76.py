2cd9940366ed9d84c33ff5493f507791b46b5139
inaszha <99781124+inaszha@users.noreply.github.com>
2023-12-24 05:50:03 +0800

    day76.py

---
+++ day76.py
@@ -0,0 +1,10 @@
+#mengakses item pada dictionary
+biodata = dict(nama="inaszhaa", hobi="membaca", pekerjaan="memantaskan diri")
+print(biodata["nama"])
+print(biodata["pekerjaan"])
+
+#mengakses semua key dan value dictionary
+semua_kunci = biodata.keys()
+semua_nilai = biodata.values()
+print(semua_kunci)
+print(semua_nilai⁰
