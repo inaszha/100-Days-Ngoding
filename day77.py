761767f153b2f691ccb40f43224a3e68eed4cae7
inaszha <99781124+inaszha@users.noreply.github.com>
2023-12-25 23:02:16 +0800

    day77.py

---
+++ day77.py
@@ -0,0 +1,12 @@
+komputer = 55000000
+pendapatan = 1500000
+bulan = 0
+tabungan = 0
+while True:
+	if tabungan < komputer:
+		tabungan+=pendapatan
+		bulan+=1
+	else:
+		break
+print(bulan)
+print(tabungan
