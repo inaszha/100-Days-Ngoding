7f977c4e1d3cea09c4a849d256afde8aee2452d2
inaszha <99781124+inaszha@users.noreply.github.com>
2024-01-06 23:48:04 +0800

    day89.py

---
+++ day89.py
@@ -0,0 +1,10 @@
+def masukkan_angka(batas):
+	angka = []
+	for i in range(batas):
+		nilai = int(input("nilai: "))
+		angka.append(nilai)
+		angka.sort
+	return angka
+
+angka = masukkan_angka(int(input("batas: ")))
+print(angka)
